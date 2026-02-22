8749e165638d4d3c4087a169ddea49ba9dc348da
Pravinshewale1 <shewalepravinkumar@gmail.com>
2026-02-22 17:49:00 +0530

    Delete test/widget_test.dart

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:satark_one/main.dart';
-
-void main() {
-  testWidgets('App launches', (tester) async {
-    await tester.pumpWidget(const SatarkOneApp());
-    expect(find.byType(SatarkOneApp), findsOneWidget);
-  });
-}
